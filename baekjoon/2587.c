ebfc2837d3abb27da8c0d26b74e22ce185b12274
yi-barrack <imdavid12@naver.com>
2023-09-18 15:44:31 +0900

    2587.c

---
+++ baekjoon/2587.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int arr[5]={0,};
+    int sum=0;
+    for(int i=0; i<5; i++){
+        scanf("%d", &arr[i]);
+        sum+=arr[i];
+    }
+    int avg = sum/5;
+    for (int i = 0; i < 5;i++){
+        for (int j = i + 1; j < 5;j++){
+            if(arr[i]>arr[j]){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    printf("%d\n%d", avg, arr[2]);
+    return 0;
+}
