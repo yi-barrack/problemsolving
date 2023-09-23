c4cebbf797e8084795715be524ff46ec3af31f84
yi-barrack <imdavid12@naver.com>
2023-09-23 23:55:41 +0900

    asdf

---
+++ baekjoon/asdf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+int main() {
+	long long a, b, c;
+	scanf("%ld %ld %ld", &a, &b, &c);
+	long long sum = a + b + c;
+	printf("%ld", sum);
+	return 0;
+}
