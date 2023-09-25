e130cdf7b6c28e0532152e1ef09bf557489a3542
yi-barrack <imdavid12@naver.com>
2023-09-25 15:15:19 +0900

    ㅇㄱㄹㅈ ㅅㅇ

---
+++ baekjoon/24263.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+    int n = 0;
+    scanf("%d", &n);
+    printf("%d\n1", n);
+
+    return 0;
+}
---
+++ baekjoon/24264.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    long long n = 0;
+    scanf("%ld", &n);
+    printf("%ld\n", n * n);
+    printf("2");
+    return 0;
+}
