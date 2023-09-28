9c7231c3269c5074499941abfab961a4523e1c5f
yi-barrack <imdavid12@naver.com>
2023-09-28 22:21:56 +0900

    25314.c

---
+++ baekjoon/25314.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    n /= 4;
+    for (int i = 0; i < n;i++){
+        printf("long ");
+    }
+    printf("int");
+    return 0;
+}
---
+++ baekjoon/4949.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef struct Stack{
+    char s[101];
+} Stack;
+
+int main(){
+    Stack *stack = (Stack *)malloc(sizeof(Stack));
+    char ch[101] = {
+        '\0',
+    };
+    while(1){
+    scanf("%s",ch);
+    if 
+    }
+
+
+    return 0;
+}
+
+
+
+
