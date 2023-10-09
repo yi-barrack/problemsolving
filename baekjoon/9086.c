05c0c34a1f541a1d19498fa13f8d6d24601b20d4
yi-barrack <imdavid12@naver.com>
2023-10-09 22:07:50 +0900

    9086.c

---
+++ baekjoon/9086.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    int n;
+    char ch[1001];
+    scanf("%d", &n);
+    for (int i = 0; i < n;i++){
+        scanf("%s", ch);
+        int r = strlen(ch);
+        printf("%c%c\n", ch[0], ch[r - 1]);
+    }
+    return 0;
+}
