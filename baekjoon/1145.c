b818de6583a16bcf9d8ef0ea7e125a840806b25a
YIBYUNGYOUNG <57635173+yi-barrack@users.noreply.github.com>
2023-12-10 23:47:43 +0900

    Update 1145.c

---
+++ baekjoon/1145.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int s[5];
+    int result = 0;
+    for (int a = 0; a < 5; a++)
+    {
+        scanf("%d", &s[a]);
+    }
+    for (int a=1;; a++)
+    {
+        for (int b = 0; b < 5; b++)
+        {
+            if (a%s[b] == 0)
+                result++;
+        }
+        if (result >= 3)
+        {
+            printf("%d\n", a);
+            break;
+        }
+        else
+            result = 0;
+    }
+    return 0;
+}
